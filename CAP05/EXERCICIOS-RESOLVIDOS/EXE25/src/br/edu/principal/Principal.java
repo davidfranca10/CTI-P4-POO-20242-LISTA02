f4c0166c2723e803e4c439d010b846213b5bff12
David França <david.franca10@aluno.ifce.edu.br>
2024-11-24 17:44:52 -0300

    Principal.java

--- CAP05/EXERCICIOS-RESOLVIDOS/EXE25/src/br/edu/principal/Principal.java
+++ CAP05/EXERCICIOS-RESOLVIDOS/EXE25/src/br/edu/principal/Principal.java
@@ -1,53 +1,69 @@
 package br.edu.principal;
 
+import java.util.Scanner;
+
 public class Principal {
 
-    public static void main(String[] args) {
-      double angulo, voltas;
-      
-      
-      angulo = 17;
-      
-      if(angulo > 360 || angulo < -360) {
-    	  voltas = (int) (angulo/360);
-    	  angulo = angulo % 360;
-      } else {
-    	  voltas = 0;
-      }
-      
-      if (angulo == 0 || angulo == 90 || angulo == 180 || angulo == 270 || angulo == 360 || angulo == -90 || angulo == -180 || angulo == -270 || angulo == -360) {
-    	    
-    	  System.out.println("Está em cima de algum dos eixos");
-    	}
-      
-      if((angulo > 0 && angulo < 90) || (angulo < -270 && angulo > -360)) {
-	  System.out.println("1º Quadrante");
-      }
-      
-	 if((angulo > 90 && angulo < 180) || (angulo < -180 && angulo > -270)) {
-		  System.out.println("2º Quadrante");
-	      }
-	 if((angulo > 180 && angulo < 270) || (angulo < -90 && angulo > -180)) {
-		  System.out.println("3º Quadrante");
-     }
-	 
-	 if((angulo > 270 && angulo < 360) || (angulo < 0 && angulo > -900)) {
-		  System.out.println("4º Quadrante");
-     }
-	 
-	 
-	  System.out.println(voltas + ":  volta(s) no sentido");
-	  
-	  if(angulo < 0) {
-		  System.out.print("horário");
-		 
-	  } else {
-		  System.out.print("anti-horário");
-	  }
-
-      
-      
-    
- }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double preco, imp, transp=0, seguro, fin, total_imp=0, origem;
+		char meio_t = ' ', carga = ' ';
+		
+		System.out.println("Preço (0 para encerrar) : ");
+		preco = sc.nextDouble();
+		while (preco>0) {
+			System.out.println("País de origem: ");
+			System.out.println("1 - EUA | 2 - México | 3 - outros");
+			origem = sc.nextDouble();
+			System.out.println("Meio de transporte: ");
+			System.out.println("T - terrestre | F - fluvial | A - aéreo");
+			meio_t = sc.next().charAt(0);
+			System.out.println("Carga perigosa?: (S/N)");
+			carga = sc.next().charAt(0);
+			if (preco<=100) {
+				imp = preco *(5/100);
+			}
+			else {
+				imp = preco *(10/100);
+			}
+			if(carga == 'S') {
+				if(origem == 1) {
+					transp = 50;
+				}
+				else if(origem == 2) {
+					transp = 21;
+				}
+				else if(origem == 3) {
+					transp = 24;
+				}
+			}
+			if (carga == 'N') {
+				if(origem == 1) {
+					transp = 12;
+				}
+				else if(origem ==2) {
+					transp = 21;
+				}
+				else if(origem == 3) {
+					transp = 60;
+				}
+			}
+			if (origem == 2 | meio_t == 'A') {
+				seguro = preco/2;
+			}
+			else {
+				seguro = 0;
+			}
+			fin = preco + imp + transp + seguro;
+			total_imp = total_imp + imp;
+			System.out.println("impostp: "+imp);
+			System.out.println("transporte : "+transp);
+			System.out.println("seguro: "+seguro);
+			System.out.println("final: "+fin);
+			System.out.println("preço (0 para encerrar): ");
+			preco = sc.nextDouble();
+			}
+		System.out.println("Total de impostos: "+total_imp);
+	}
 
 }
