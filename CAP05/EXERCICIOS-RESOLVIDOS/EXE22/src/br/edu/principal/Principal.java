1390b68a6080b811f710eb23d44570aae128f65a
David França <david.franca10@aluno.ifce.edu.br>
2024-11-24 17:49:34 -0300

    Principal.java

--- CAP05/EXERCICIOS-RESOLVIDOS/EXE22/src/br/edu/principal/Principal.java
+++ CAP05/EXERCICIOS-RESOLVIDOS/EXE22/src/br/edu/principal/Principal.java
@@ -1,74 +1,68 @@
 package br.edu.principal;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		double sal_base, tempo, imposto, grat;
-		double sal_liq;
-		
-		sal_base = 1406;
-		tempo = 10;
-		
-		if(sal_base > 200) {
-			
-			imposto = 0;
-		} else if(sal_base <= 450) {
-			imposto =  3/100 * sal_base;
-			
-		} else if(sal_base < 700) {
-			imposto = 8/100 * sal_base;
-			
-			
-		} else {
-			imposto = 12/100 * sal_base;
+		Scanner sc = new Scanner(System.in);
+		double  tot_f=0, tot_m=0, cont_ml=00, tot=0, perc;
+		int menor_idade = 0, idade, media_idade, soma1=0,  cont_m2=0, cont_fl=0;
+		char sexo=' ', exp=' ';
+		System.out.println("Sua idade (0 para encerrar);");
+		idade = sc.nextInt();
+		while(idade !=0) {
+			System.out.println("Sexo:");
+			System.out.println("M - Masculino | F - feminino");
+			sexo = sc.next().charAt(0);
+			System.out.println("Tem experiência  no serviço? (S/N)");
+			exp = sc.next().charAt(0);
+			if (sexo == 'F' & exp == 'S') {
+				if (tot == 0) {
+					menor_idade = idade;
+					tot = 1;
+				}
+				else if (idade < menor_idade) {
+					menor_idade = idade;
+				}
+			}
+			if (sexo == 'M') {
+				tot_m += 1;
+			}
+			if (sexo == 'F') {
+				tot_f += 1;
+			}
+			if (sexo == 'F' & idade < 21 & exp == 'S') {
+				cont_fl +=1;
+			}
+			if (sexo == 'M' & idade > 45) {
+				cont_ml +=1;
+			}
+			if (sexo == 'M' & exp == 'S') {
+				soma1 += idade;
+				cont_m2 += 1;
+			}
+			System.out.println("Digite idade (0 para encerrar):");
+			idade = sc.nextInt();
 		}
-		
-		System.out.println(imposto);
-		
-		if(sal_base > 500) {
-			if(tempo <= 3) {
-				grat = 20;
-				
-			} else {
-				
-				grat = 30;
-			}
-			
-			if(tempo <= 3) {
-				grat = 23;
-				
-				
-			}
-			
-			else if(tempo < 6) {
-				grat = 35;
-				
-			} else {
-				grat = 33;
-			}
-			
-			System.out.println(grat);
-			
-			sal_liq = sal_base - imposto + grat;
-			
-			if(sal_liq <= 350) {
-				System.out.println("Classificação A");
-				
-				
-			}
-			
-			else if(sal_liq < 600) {
-				System.out.println("Classificação B");
-
-			} else {
-				
-				System.out.println("Classificação C");
-
-			}
-			
+		System.out.println("Total de candidatas: "+tot_f);
+		System.out.println("Total de canditados: "+tot_m);
+		if (cont_m2 == 0) {
+			System.out.println("Nenhum homem com experiência");
 		}
-		
-		
-	}
+		else {
+			media_idade = soma1 / cont_m2;
+			System.out.println("Idade média dos homens que ja tem experiência no serviço: "+media_idade+" anos");
+		}
+		if (tot_m==0) {
+			System.out.println("Nenhum homem");
+		}
+		else {
+			perc = cont_ml * 100/tot_m;
+			System.out.println("Porcentagem de homens com mais de 45 anos:  "+perc+"%");
+		}
+		System.out.println("Número de mulheres com a idade inferior a 21 anos(com experiência no serviço): "+cont_fl);
+		System.out.println("Menor idade entre as mulheres com experiência: "+menor_idade+" anos");
+		}
 
 }
